27f8242427a47b69d821b8b5599cce435ce5d36a
nikolastimoteo <nikolastps@hotmail.com>
2017-12-18 23:20:26 -0200

    Correções no Layout e registro de post do tipo Produto

--- functions.php
+++ functions.php
@@ -34,3 +34,43 @@ function wp_responsivo_scripts() {
   wp_enqueue_script('bootstrap-js', get_template_directory_uri().'/assets/js/bootstrap.min.js', array('jquery-slim'), '', true  );
 }
 add_action( 'wp_enqueue_scripts', 'wp_responsivo_scripts' );
+
+/**************************************
+ * Registro Custom Post type Produtos
+ **************************************/
+add_action('init', 'register_post_produtos');
+function register_post_produtos() { 
+  $labels = array(
+    'name' => _x('Produtos', 'post type general name'),
+    'singular_name' => _x('Produto', 'post type singular name'),
+    'add_new' => _x('Adicionar Produto', 'Novo Produto'),
+    'add_new_item' => __('Novo Produto'),
+    'edit_item' => __('Editar Produto'),
+    'new_item' => __('Novo Produto'),
+    'view_item' => __('Ver Produto'),
+    'search_items' => __('Procurar Produtos'),
+    'not_found' =>  __('Nenhum registro encontrado'),
+    'not_found_in_trash' => __('Nenhum registro encontrado na lixeira'),
+    'parent_item_colon' => '',
+    'menu_name' => 'Produtos'
+  );
+
+  $args = array(
+    'labels' => $labels,
+    'public' => true,
+    'public_queryable' => true,
+    'show_ui' => true,           
+    'query_var' => true,
+    'rewrite' => true,
+    'has_archive' => true,
+    'menu_icon' => 'dashicons-pressthis',
+    'capability_type' => 'post',
+    'hierarchical' => false,
+    'rewrite' => array('slug'=>'produtos'), 
+    'menu_position' => 5,
+    'supports' => array('title','editor','thumbnail'),
+  );
+ 
+  register_post_type( 'produtos' , $args );
+  flush_rewrite_rules();
+}
--- index.php
+++ index.php
@@ -1,14 +1,37 @@
 <?php get_header(); ?>
 
-<div id="carouselExampleSlidesOnly" class="carousel carousel-custom">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="<?php bloginfo('template_directory');?>/assets/images/slider-01.jpg" alt="Backgroud Docas">
-	  <div class="carousel-caption d-md-block-custom">
-	    <img class="d-block w-custom" src="<?php bloginfo('template_directory');?>/assets/images/logo.png" alt="Logo da Empresa">
+<div class="slider">
+	<div id="carouselExampleSlidesOnly" class="carousel carousel-custom">
+	  <div class="carousel-inner">
+	    <div class="carousel-item active">
+	      <img class="d-block w-100" src="<?php bloginfo('template_directory');?>/assets/images/slider-01.jpg" alt="Backgroud Docas">
+		  <div class="carousel-caption d-md-block-custom">
+		    <img class="d-block w-custom" src="<?php bloginfo('template_directory');?>/assets/images/logo.png" alt="Logo da Empresa">
+		  </div>
+	    </div>
 	  </div>
-    </div>
-  </div>
+	</div>
 </div>
+
+<div class="produtos">
+	<div class="container">
+		<div class="row">
+			
+			<div class="col-lg-4 col-sm-6">
+				<div class="row">
+				<div class="col-4 produto-imagem">
+				    <img class="top img-fluid" src="<?php bloginfo('template_directory');?>/assets/images/slider-01.jpg" alt=""/>
+				</div>
+				<div class="col-8 produto-info">
+				    <h2 class="descricao">Marlin Azul</h2>
+				    <p class="preco">R$ 100,00/kg</p>
+				</div>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+</div>
+
 
 <?php get_footer(); ?>
